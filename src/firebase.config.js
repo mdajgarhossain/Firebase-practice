dba76ed392643e6e03d10825a86b9cc86d676a7c
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-26 19:29:02 +0600

    Step 2: Add firebaseConfig file

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAOV8lNVtrddAnwmRDFIOx290SLsryQfOs",
+    authDomain: "ema-john-shop.firebaseapp.com",
+    databaseURL: "https://ema-john-shop.firebaseio.com",
+    projectId: "ema-john-shop",
+    storageBucket: "ema-john-shop.appspot.com",
+    messagingSenderId: "752245847387",
+    appId: "1:752245847387:web:cde47aca149e294400d67e"
+  };
+
+  export default firebaseConfig;
